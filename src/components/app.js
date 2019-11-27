200b42bcf9a8e4397dc5ab1b9c7cc3766b77c508
gianMarquez <michimar.alexis@gmail.com>
2019-11-27 17:47:35 -0600

    fixed ring seven logo

--- src/components/app.js
+++ src/components/app.js
@@ -19,7 +19,7 @@ import dashLogo from "./images/logos/Artboard-4.png";
 import villageLogo from "./images/logos/village-logo.png";
 import bfLogo from "./images/logos/bussines-logo.png";
 import bfWhiteLogo from "./images/logos/bfi-logo-1.png";
-// import ringSevenLogo from "./images/logos/ring-seven-logo.png";
+import ringSevenLogo from "./images/logos/ring-seven-logo.png";
 import bpuLogo from "./images/logos/BPU-Logo.png";
 
 import filiImage from "./images/team/Fili.jpg";
